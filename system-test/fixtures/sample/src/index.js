ce9f648643d30045d16c60370e4aef63719fa917
Sofia Leon <sofialeon@google.com>
2021-06-10 13:44:36 -0500

    fix: system name

--- system-test/fixtures/sample/src/index.js
+++ system-test/fixtures/sample/src/index.js
@@ -17,7 +17,7 @@
 // ** All changes to this file may be overwritten. **
 
 /* eslint-disable node/no-missing-require, no-unused-vars */
-const essentialcontacts = require('@google-cloud/essentialcontacts');
+const essentialcontacts = require('@google-cloud/essential-contacts');
 
 function main() {
   const essentialContactsServiceClient =
